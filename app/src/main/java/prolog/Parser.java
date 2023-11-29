7258fe7320ea0cb0612e8bd78dd244f86b162dde
Lorenz aka Lolo Hänggi <lolo8304@gmail.com>
2023-11-29 08:52:28 +0100

    Update Parser.java

---
+++ app/src/main/java/prolog/Parser.java
@@ -0,0 +1,13 @@
+Package prolog;
+
+public class Parser {
+
+    public Parser(Lexer lexer) { 
+        this.lexer = lexer;
+    }
+
+    public Node parse() {
+        return parseProgram() {
+        }'
+    }
+}
