afd81794c2313a436083d5a850ad1f6c62a8ca82
Lorenz aka Lolo Hänggi <lolo8304@gmail.com>
2023-11-29 09:10:53 +0100

    Update Parser.java
    parse program

--- app/src/main/java/prolog/Parser.java
+++ app/src/main/java/prolog/Parser.java
@@ -6,8 +6,24 @@ public class Parser {
         this.lexer = lexer;
     }
 
-    public Node parse() {
-        return parseProgram() {
-        }'
+    // (* A Prolog program is a series of clauses *)
+    //program = { clause, "." } ;
+    public ProgramNode parse() {
+        return parseProgram();
     }
+
+    public ProgramNode parseProgram() {
+        var node = new ProgamNode();
+        var clause = this parseClause();
+        while (node.isPresent()) {
+            var dot = this.lexer.next();
+            if (dot.token != Token.DOT) {
+                throw new IOException("missing . at end of clause. received "+dot+;);
+            }
+            clause = this.parseClause();
+        }
+    }
+
+    //* A clause can be a fact, a rule, or a query *)
+    //clause = fact | rule | query ;
 }
